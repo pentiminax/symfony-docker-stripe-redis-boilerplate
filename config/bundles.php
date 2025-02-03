43d879fa03e3cc10b21a0bb95649b8fe73856973
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-02-03 11:14:20 +0100

    Add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Talleu\RedisOm\Bundle\TalleuRedisOmBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
